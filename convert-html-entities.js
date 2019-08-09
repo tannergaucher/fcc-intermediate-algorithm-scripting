8bf938faabf54be9def6150ff1478b1245bb07cf
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-08-09 14:45:40 -0400

    Update convert-html-entities.js

--- convert-html-entities.js
+++ convert-html-entities.js
@@ -2,36 +2,36 @@
 
 Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
+TODO
 */
 
-function convertHTML(str) {
-  const table = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&apos;',
-  }
+// function convertHTML(str) {
+//   const table = {
+//     '&': '&amp;',
+//     '<': '&lt;',
+//     '>': '&gt;',
+//     '"': '&quot;',
+//     "'": '&apos;',
+//   }
 
-  //   change to loop
-  for (let i = 0; i < str.length; i++) {
-    let currentLetter = str[i]
+//   for (let i = 0; i < str.length; i++) {
+//     let currentLetter = str[i]
 
-    console.log(currentLetter)
+//     console.log(currentLetter)
 
-    if (
-      currentLetter === '&' ||
-      currentLetter === '<' ||
-      currentLetter === '>' ||
-      currentLetter === '"' ||
-      currentLetter === "'"
-    ) {
-      str = str.replace(currentLetter, table[currentLetter])
-    }
-    i -= table[currentLetter].length()
-  }
+//     if (
+//       currentLetter === '&' ||
+//       currentLetter === '<' ||
+//       currentLetter === '>' ||
+//       currentLetter === '"' ||
+//       currentLetter === "'"
+//     ) {
+//       str = str.replace(currentLetter, table[currentLetter])
+//     }
+//     i -= table[currentLetter].length()
+//   }
 
-  return str
-}
+//   return str
+// }
 
 convertHTML('Dolce & Gabbana')
